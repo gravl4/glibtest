6dc0be3eb9a9acb1f6bd009dcd23b6383ae338f5
gravl4 <gravl4@gmail.com>
2021-08-25 22:59:59 +0700

    f commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+#[allow(dead_code)] // отключаем предупреждение о неиспользуемом коде
+mod m_gitlib {
+    pub fn m_lib_test() {
+        println!("LIB FROM GITHUB")
+    }
+}
